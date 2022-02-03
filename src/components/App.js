fa7b818990160e9047c8ae71557124a3235fd1f4
Westin Humble <westin.h@gmail.com>
2022-02-03 10:36:24 -0500

    switching drivers

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,10 @@
+import React from "react";
+//import Comment from "./Comments";
+import Likes from "./Likes";
 import video from "../data/video.js";
+
+
+
 
 function App() {
   console.log("Here's your data:", video);
@@ -8,11 +14,12 @@ function App() {
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src= "https://www.youtube.com/embed/dpw9EHDh2bM"
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
       />
+      UpVotes()
     </div>
   );
 }
---
+++ src/components/Likes.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import React, { useState } from "react";
+
+
+function UpVotes() {
+    return (
+        <div id= "buttons">
+            <button className= {Upvote}></button>
+            <button className = {Downvote}></button>
+        </div>
+    )
+
+}
+
+export default Likes;
